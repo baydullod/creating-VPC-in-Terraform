934f9369ed73a15c1847c29247a8aaeb67fc7d90
Baydullo <baydullod@gmail.com>
2021-05-12 12:01:26 +0000

    updated

--- data.tf
+++ data.tf
@@ -2,5 +2,5 @@ data "aws_availability_zones" "available" {
     state = "available"
 }
 output "AZ" {
-    value = "data.aws_availability_zones.names" 
+    value = data.aws_availability_zones.available.names [0]
     }
