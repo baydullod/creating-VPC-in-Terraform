18f10945ac41e16c1bfca6559c5c06f7165ba3c6
Baydullo <baydullod@gmail.com>
2021-05-12 12:58:26 +0000

    added assosiation

--- public_subnet.tf
+++ public_subnet.tf
@@ -16,3 +16,27 @@ resource "aws_subnet" "public_subnet3" {
  availability_zone = data.aws_availability_zones.available.names [2]
   tags = var.tags
 }
+resource "aws_internet_gateway" "gw" {
+    vpc_id = aws.main.id
+    tags = var.tags
+}
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.gw.id
+    }
+    tags = var.tags
+}
+resource "aws_route_table_association" "private1" {
+    subnet_id = aws.subnet.private1.id
+    aws_route_table_id = aws_route_table.privavate.id
+}
+resource "aws_route_table_association" "private2" {
+    subnet_id = aws.subnet.private2.id
+    aws_route_table_id = aws_route_table.privavate.id
+}
+resource "aws_route_table_association" "private3" {
+    subnet_id = aws.subnet.private3.id
+    aws_route_table_id = aws_route_table.privavate.id
+}
